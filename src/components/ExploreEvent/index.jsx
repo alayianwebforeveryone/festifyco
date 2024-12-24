d5b6277174dd1065f010371dc8102b8881c2a084
H-Anjum8 <huma.anjum88@gmail.com>
2024-12-24 11:27:49 +0500

    NavBar and Login

---
+++ src/components/ExploreEvent/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExploreEvent = () => {
+  return (
+    <div>ExploreEvent</div>
+  )
+}
+
+export default ExploreEvent
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <>
+  <div className=' container h-[90px]  mb-0 pb-0 '>
+        <p className='py-[45px]  flex  content-center justify-center'>Copyright all right reserved 2025</p>
+
+  </div>
+  
+  </>
+  )
+}
+
+export default Footer
