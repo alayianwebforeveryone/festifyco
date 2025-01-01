7b291e9a23e9135f570b45dca09ad2925ffb50f3
H-Anjum8 <huma.anjum88@gmail.com>
2025-01-01 19:54:10 +0500

    createEveForm

---
+++ src/components/Dashboard/CreateEvent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateEventForm from './CreateEventForm'
+
+const CreateEvent = () => {
+  return (
+    <>
+    <CreateEventForm/>
+    </>
+  )
+}
+
+export default CreateEvent
